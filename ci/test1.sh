47c6c83297a0bb8612f92799755a7b323e8be1f4
Jon Siwek <jsiwek@corelight.com>
2019-12-12 14:15:13 -0800

    Test secure var

--- ci/test1.sh
+++ ci/test1.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
 
-set -x
-
-echo hello test1: ${CIRRUS_OS}
-nproc
-free -h
-df -h
-
-sysctl -a | grep machdep.cpu
-
-which pip
+echo "${SSH_KEY}" > cirrus_key.b64
+base64 --decode cirrus_key.b64 > cirrus_key
+rm cirrus_key.b64
+chmod 600 cirrus_key
+mkdir -p ~/.ssh
+mv cirrus_key ~/.ssh/id_rsa
+echo "Host *" >> ~/.ssh/config
+echo "    StrictHostKeyChecking no" >> ~/.ssh/config
+chmod 400 ~/.ssh/config
+git clone git@github.com:jsiwek/cirrus-ci-test
+rm ~/.ssh/id_rsa
+ls cirrus-ci-test
 
 exit 0
